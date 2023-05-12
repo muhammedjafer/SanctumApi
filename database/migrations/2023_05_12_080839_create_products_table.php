48b03bd58dc6ab8a53a6e7a72bf44e5268e86258
Muhammed <mohammadjafer990@gmail.com>
2023-05-12 12:48:48 +0300

    finished the crud api

--- database/migrations/2023_05_12_080839_create_products_table.php
+++ database/migrations/2023_05_12_080839_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price', 5, 2);
+            $table->string('slug');
             $table->timestamps();
         });
     }
